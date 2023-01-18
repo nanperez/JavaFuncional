9b2c4432cb0ef68e00290067e1ab42e1f86d4384
Nancy <perez.castro.nancy@gmail.com>
2023-01-18 13:47:41 -0600

    Add files via upload

---
+++ FuncionesAnonimas/src/funcionesanonimas/FuncionesAnonimas.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package funcionesanonimas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.*;
+
+/**
+ *
+ * @author naperez
+ */
+public class FuncionesAnonimas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        List<Integer> numeros = List.of(10,5,3,7,8,22);
+        List<Integer> pares = new ArrayList<Integer>();
+        // Versión imperativa
+        //for(Integer n:numeros){
+        //    if(n%2==0)
+       //         pares.add(n);
+        //}        
+        //versión funcional
+        pares = pares.stream(filter())
+        
+        
+        
+        
+    }
+    
+    private static 
+    
+}
